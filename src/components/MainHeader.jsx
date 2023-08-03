7669770fb6e9b0b90ac82809f11f01bdd8a4589b
wakana <wakanawardrobe@gmail.com>
2023-08-03 14:50:17 +0100

    add basic image header

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,11 @@
+
+const MainHeader = () => {
+
+    return (
+        <header className="header">
+            <img className="header__image"src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80" alt="header-image" />
+        </header>
+    )
+}
+
+export default MainHeader;
