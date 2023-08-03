b81fb897de12f769f3e8ed87daad3acd7731d2de
wakana <wakanawardrobe@gmail.com>
2023-08-03 16:11:50 +0100

    add scaleable heading and down icon

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,9 +1,17 @@
+import Navbar from "./Navbar";
+import downIcon from '../assets/icons/down.svg';
 
 const MainHeader = () => {
 
+    const backgroundUrl = "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80";
+
     return (
         <header className="header">
-            <img className="header__image"src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww&w=1000&q=80" alt="header-image" />
+            <Navbar />
+            <h1 className="main-heading absolute left-10 top-20 text-neutral-950">Personal <br></br>
+            <span>Portfolio</span></h1>
+            <img className="header__image"src={backgroundUrl} alt="header-image" />
+            <img className=" down-icon absolute bottom-5 left-1/2 w-10" src={downIcon} alt="direct-down-icon"/>
         </header>
     )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar">
+        <ul>
+            <li 
+            className="bg-neutral-950 text-white font-semibold px-2 py-1 rounded-sm">
+                <button>PROJECTS</button>
+            </li >
+            <li 
+            className="bg-neutral-950 text-white font-semibold px-2 py-1 rounded-sm">
+                <button>ABOUT</button>
+            </li>
+            <li 
+            className="bg-neutral-950 text-white font-semibold px-2 py-1 rounded-sm">
+                <button>CONTACT</button>
+            </li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
