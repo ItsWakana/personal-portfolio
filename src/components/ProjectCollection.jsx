6ef36d314cbbc6c52470d2939c1d77f89af4e5bb
wakana <wakanawardrobe@gmail.com>
2023-08-09 11:34:20 +0100

    add media queries for grid padding

--- src/components/ProjectCollection.jsx
+++ src/components/ProjectCollection.jsx
@@ -5,7 +5,7 @@ const ProjectCollection = () => {
 	return (
 		<section id="projects" className="text-center pt-5">
 			<h2 className="text-4xl font-semibold">My Projects</h2>
-			<div className="project-container grid gap-8 md:grid-cols-2 grid-cols-1">
+			<div className="grid gap-8 md:grid-cols-2 grid-cols-1 lg:mx-40 mx-8 my-6">
 				{projectData.map((project) => (
                     <Project 
                     key={project.id} 
