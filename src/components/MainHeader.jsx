ad493931b78e49534bbe76b6978cd5a571233e6d
wakana <wakanawardrobe@gmail.com>
2023-08-09 15:55:27 +0100

    adjust gradient colours

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -9,7 +9,7 @@ const MainHeader = () => {
             <div className="w-1/3 h-2/4 bg-gradient-to-r from-amber-600 to-red-600 absolute right-0 bottom-0 rounded-tl-[400px]"></div>
             <div className="w-[80px] h-[80px] bg-gradient-to-r from-amber-600 to-red-600 absolute left-0 top-0 rounded-br-[400px]"></div>
                 <div className="flex flex-col text-center md:text-left md:ml-10">
-                    <p className="text-white">WELCOME TO MY PORTFOLIO</p>
+                    <p className="text-white">WELCOME TO MY PORTFOLIO </p>
                     <h1 className="main-heading text-white">
                         My Programming <span className="text-transparent bg-gradient-to-r from-amber-600 via-orange-500 to-red-800 bg-clip-text">Journey.</span>
                     </h1>
