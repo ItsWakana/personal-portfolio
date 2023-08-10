bc060fe4ba205ca25db81363269c3b128fe920d8
wakana <wakanawardrobe@gmail.com>
2023-08-10 17:03:22 +0100

    add technologies tabs under each project card

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,8 @@
-// import placeholder from '../assets/project-placeholder.png';
 import exampleImage from '../assets/web-projectplaceholder.png';
 import githubLogo from '../assets/icons/github.svg';
 import externalLink from '../assets/icons/external-link.svg';
 import IconLink from './IconLink';
+import { techColor } from '../projectData';
 
 const Project = ( {project }) => {
 
@@ -10,12 +10,17 @@ const Project = ( {project }) => {
     <div className="project bg-gray-900 text-white rounded-lg border-2 border-gray-800" key={project.id}>
         <img className="rounded-lg" src={exampleImage} alt="project-image" />
         <div className="flex flex-col items-start gap-3 py-4">
-          <div className="flex gap-2">
+          <div className="flex gap-2 icons">
             <IconLink url={project.repoUrl} iconSrc={githubLogo} width={7}/>
             <IconLink url={project.liveUrl} iconSrc={externalLink} width={8}/>
           </div>
           <h3 className="font-semibold">{project.name}</h3>
           <p className="text-sm">{project.description}</p>
+          <ul className="flex gap-3">
+            {project.technologies.map((tech) => (
+              <li className={`${techColor[tech]} rounded-xl px-2 text-sm`}key={tech}>{tech}</li>
+            ))}
+          </ul>
         </div>
     </div>
   )
--- src/projectData.js
+++ src/projectData.js
@@ -1,8 +1,18 @@
+export const techColor = {
+    'ReactJS': 'bg-emerald-500',
+    'Tailwind CSS': 'bg-cyan-600',
+    'Firebase': 'bg-green-600',
+    'CSS': 'bg-blue-600',
+    'Javascript': 'bg-red-600',
+    'HTML': 'bg-orange-600'
+}
+
 const projectData = [
     {
         id: 1,
         name: 'Photo Tagging App',
         description: 'A game where you find the characters in the fatest time.',
+        technologies: ['ReactJS', 'Tailwind CSS', 'Firebase'],
         repoUrl: 'https://github.com/ItsWakana/Photo-tagging-app',
         liveUrl: 'https://character-finder-3b6c2.web.app/'
     },
@@ -10,6 +20,7 @@ const projectData = [
         id: 2,
         name: 'Shopping Cart',
         description: 'Interactive Shopping Cart for adding and removing video game items.',
+        technologies: ['ReactJS', 'CSS', 'Firebase'],
         repoUrl: 'https://github.com/ItsWakana/Shopping-Cart',
         liveUrl: 'https://itswakana.github.io/Shopping-Cart/'
     },
@@ -17,6 +28,7 @@ const projectData = [
         id: 3,
         name: 'Memory Game',
         description: 'Memorize the order of the cards and try not to click the same card twice!',
+        technologies: ['ReactJS', 'CSS'],
         repoUrl: 'https://github.com/ItsWakana/Memory-Game',
         liveUrl: 'https://itswakana.github.io/Memory-Game/'
     },
@@ -24,6 +36,7 @@ const projectData = [
         id: 4,
         name: 'Battleships',
         description: 'Play the classic Battleship game against the computer.',
+        technologies: ['Javascript', 'HTML', 'CSS'],
         repoUrl: 'https://github.com/ItsWakana/battleship',
         liveUrl: 'https://itswakana.github.io/battleship/'
     }
