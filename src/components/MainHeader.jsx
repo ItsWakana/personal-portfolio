592e881f83c05885087399ccb69fd20e7604078e
wakana <wakanawardrobe@gmail.com>
2023-08-11 15:22:23 +0100

    fix font scaling and shape

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,18 +7,18 @@ const MainHeader = () => {
         <header className="header bg-gray-950 flex flex-col items-center gap-12 relative">
             <Navbar />
             <div className="w-1/3 h-2/4 bg-gradient-to-r from-amber-600 to-red-600 absolute right-0 bottom-0 rounded-tl-[400px]"></div>
-            <div className="w-[80px] h-[80px] bg-gradient-to-r from-amber-600 to-red-600 absolute left-0 top-0 rounded-br-[400px]"></div>
+            <div className="w-[8vw] h-[8vh] md:w-[80px] md:h-[80px] bg-gradient-to-r from-amber-600 to-red-600 absolute left-0 top-0 rounded-br-[400px]"></div>
                 <div className="flex flex-col text-center md:text-left md:ml-10">
                     <p className="text-white">WELCOME TO MY PORTFOLIO </p>
                     <h1 className="main-heading text-white">
-                        My Programming <span className="text-transparent bg-gradient-to-r from-amber-600 via-orange-500 to-red-800 bg-clip-text">Journey.</span>
+                        My Development <span className="text-transparent bg-gradient-to-r from-amber-600 via-orange-500 to-red-800 bg-clip-text">Showcase.</span>
                     </h1>
                 </div>
-            <button className="down-icon w-10">
+            {/* <button className="down-icon w-10">
                 <a href="#projects">
                 <img src={downIcon} alt="direct-down-icon"/> 
                 </a>
-            </button>
+            </button> */}
         </header>
     )
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ const Project = ( {project }) => {
 
   return (
     <div className="project bg-gray-900 text-white rounded-lg border-2 border-gray-800 md:w-1/4 w-3/4 min-w-[250px]" key={project.id}>
-        <img className="rounded-lg" src={exampleImage} alt="project-image" />
+        <img className="rounded-lg h-[160px] object-cover" src={exampleImage} alt="project-image" />
         <div className="flex flex-col items-start gap-3 py-4 h-full">
           <div className="flex gap-2 icons">
             <IconLink url={project.repoUrl} iconSrc={githubLogo} width={7}/>
@@ -18,7 +18,7 @@ const Project = ( {project }) => {
           <p className="text-sm">{project.description}</p>
           <ul className="flex gap-2 mt-auto">
             {project.technologies.map((tech) => (
-              <li className={`${techColor[tech]} rounded-xl px-1 text-sm`}key={tech}>{tech}</li>
+              <li className={`${techColor[tech]} rounded-xl px-2 text-sm`}key={tech}>{tech}</li>
             ))}
           </ul>
         </div>
