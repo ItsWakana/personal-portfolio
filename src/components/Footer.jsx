1ef33da2858469016d4958b87c416b82b65ab315
wakana <wakanawardrobe@gmail.com>
2023-08-11 15:51:38 +0100

    add starter section for contact me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 const Footer = () => {
 
     return (
-        <footer className="bg-red-500 text-center"> 
-            <h3>Contact Me</h3>
+        <footer className="bg-gray-950 text-center pb-20"> 
+            <h3 className="text-4xl text-white font-semibold">Contact <span className="text-transparent bg-gradient-to-r from-amber-600 via-orange-500 to-red-800 bg-clip-text">Me</span></h3>
+
+            <form>
+                <label htmlFor="name">
+                    <input type="text" id="name" />
+                </label>
+            </form>
         </footer>
     )
 }
--- src/components/ProjectCollection.jsx
+++ src/components/ProjectCollection.jsx
@@ -3,7 +3,7 @@ import Project from "./Project";
 
 const ProjectCollection = () => {
 	return (
-		<section id="projects" className="pt-5 bg-gray-950 flex flex-col items-center">
+		<section id="projects" className="pt-5 pb-7 bg-gray-950 flex flex-col items-center">
 			<div className="flex flex-col text-center w-2/5 min-w-[300px]">
 			<h2 className="text-4xl font-semibold text-white">My <span className="text-transparent bg-gradient-to-r from-amber-600 via-orange-500 to-red-800 bg-clip-text">Projects</span></h2>
 			<p className="text-white mt-4"><i>I have curated a selection of my programming projects. These projects use a mixture of HTML, CSS, and JavaScript.</i></p>
